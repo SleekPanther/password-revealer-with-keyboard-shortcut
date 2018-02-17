e5e472cc3291cc0f7cbcd98b704a42a68518225d
SleekPanther <macrobyte2@gmail.com>
2018-02-17 14:40:54 -0500

    Change icon when revealed

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+let revealed = false	//keeps track of whether the password is currently shown or hidden
+
 //Reveal password when icon clicked
 chrome.browserAction.onClicked.addListener(function(tab) {
 	revealPassword()
@@ -10,10 +12,14 @@ chrome.commands.onCommand.addListener(function(command) {
 })
 
 function revealPassword(){
+	revealed = true
 	chrome.tabs.query({active: true, currentWindow: true}, (tabs)=>{
 		chrome.tabs.sendMessage(
 			tabs[0].id,
 			{msg: 'revealPassword'}
 		)
 	})
+	if(revealed){
+		chrome.browserAction.setIcon({path: "assets/icons/icon-revealed.png"})
+	}
 }
